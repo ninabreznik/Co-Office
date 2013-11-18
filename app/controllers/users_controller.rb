8a5cea7ca8766f56ce922b6c9397431f42da12be
Nina Breznik <ninabreznik@gmail.com>
2013-11-19 00:52:15 +0100

    Error message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  
 
   def new
     @user = User.new(params[:user])
@@ -7,8 +6,11 @@ class UsersController < ApplicationController
  
   def create
     @user = User.new(user_params)
-    @user.save
-    redirect_to @user
+    if @user.save
+      redirect_to @user
+    else 
+      render 'new'
+    end
   end
 
   def show
