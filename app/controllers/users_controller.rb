ca1d9b4359e80e9239096277ac67f921e5e4a101
Nina Breznik <ninabreznik@gmail.com>
2013-11-27 19:51:43 +0100

    Sign-in-out completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Pinboard!"
+      sign_in @user
+      flash[:success] = "Welcome to the Co-Office"
       redirect_to @user
     else 
       render 'new'
--- spec/requests/static_pages_home_spec.rb
+++ spec/requests/static_pages_home_spec.rb
@@ -7,7 +7,7 @@ describe "Static pages" do
   describe "home page" do
     before { visit home_path }
 
-    it { should have_content('Pinboard') }
+    it { should have_content('Co-Office') }
     it { should have_link('SignUp', href: signup_path) }
     it { should have_link('SignIn', href: signin_path) }
   end
